7764cf1fab65071ec4593716802aa9d389cb6001
f11m4at4 <f11m4at4@gmail.com>
2021-07-16 17:21:47 +0900

    20210716
    Gamemode FSM 열거형만들기

--- Source/CPP_Shooting/public/CPP_ShootingGameModeBase.h
+++ Source/CPP_Shooting/public/CPP_ShootingGameModeBase.h
@@ -6,6 +6,16 @@
 #include "GameFramework/GameModeBase.h"
 #include "CPP_ShootingGameModeBase.generated.h"
 
+// 열거형
+// 특정한 숫자를 의미있는 데이터로 만들어주는 사용자 자료형c
+UENUM(BlueprintType)
+enum class EGameState : uint8
+{
+	Ready UMETA(DisplayName = "READY STATE"),
+	Playing UMETA(DisplayName = "PLAYING STATE"),
+	Gameover UMETA(DisplayName = "GAMEOVER STATE")
+};
+
 /**
  * 
  */
@@ -16,6 +26,8 @@ class CPP_SHOOTING_API ACPP_ShootingGameModeBase : public AGameModeBase
 	
 public:
 	ACPP_ShootingGameModeBase();
-
-	//static int a;
+	
+	//int state = 0;
+	UPROPERTY(EditAnywhere, Category="FSM", BlueprintReadWrite)
+	EGameState state = EGameState::Ready;
 };
